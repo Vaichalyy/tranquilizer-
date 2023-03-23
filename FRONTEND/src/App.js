251f375f33af298690c9d1c2b4b3088890d45812
Vaichalyy <125038887+Vaichalyy@users.noreply.github.com>
2023-03-23 05:44:19 +0530

    Add files via upload

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register';
+import Logout from './components/Logout';
+
+
+import { Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/service" element={<Services/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/logout" element={<Logout/>}></Route>
+
+
+   </Routes>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
