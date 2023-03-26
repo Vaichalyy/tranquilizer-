25207a5d8779ff512631ac875efb982ff01fee72
Smriti <smritipremkumar21@gmail.com>
2023-03-26 18:46:20 +0530

    to create input for graph

--- MODEL/classifier.py
+++ MODEL/classifier.py
@@ -5,6 +5,10 @@ from keras.models import load_model
 model=load_model('tranquilizer-\MODEL\model_file2_70epochs.h5')
 video=cv2.VideoCapture(0)
 
+file = open('state_values.txt', 'w')
+state_label = None
+
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 labels_dict={0:'Anxiety',1:'Neutral'}
@@ -21,6 +25,15 @@ while True:
         result=model.predict(reshaped)
         label=np.argmax(result, axis=1)[0]
         print(label)
+
+        state_label = str(label)
+    
+
+        file = open('state_values.txt', 'a')
+        file.write(state_label+'\n') # save the status to a txt file to further usage
+        file.close()
+
+
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
         cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
