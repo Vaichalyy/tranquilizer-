bc7ffbb643c2ea4401a2ebaba7500e493a2c02e4
Smriti <smritipremkumar21@gmail.com>
2023-04-02 19:59:56 +0530

    added Detect.jsx

---
+++ FRONTEND/src/components/Detect.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import VideoStream from '../VideoStream';
+
+const Detect = () => {
+
+  return (
+    <div>
+      <section id="detect">
+        <div className="container my-5 py-5">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="fs-5 text-center mb-0">Classify</h3>
+              <h1 className="display-6 text-center mb-4">
+                Our <b>Awesome</b>Classiy
+              </h1>
+              <hr className="w-15 mx-auto" />
+            </div>
+          </div>
+          <div>
+            <h1>My App</h1>
+            <VideoStream />
+          </div> 
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Detect;
